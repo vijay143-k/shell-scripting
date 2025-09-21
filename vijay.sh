57345f6dec236ffe78e8e388858127867cc23789
vijay143-k <vijaykumarmoddu8919@gmail.com>
2025-09-21 23:33:22 +0530

    save the chnages

---
+++ vijay.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "vijju:: hi kavitha how are u"
+echo "kavitha:: hi vijju i am fine what about you"
+echo "vijju:: i am good thanks for asking what are you doing"
+echo "kavitha:: nothing just watching movies"
+
+
