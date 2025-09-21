61ecf3efdbdcffc7399e666311fc18292ae255bf
vijay143-k <vijaykumarmoddu8919@gmail.com>
2025-09-22 00:45:50 +0530

    save the chnages

--- special-vars.sh
+++ special-vars.sh
@@ -4,3 +4,7 @@ echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
 
 echo "script-name: $0"
+
+echo "current directory name: $PWD"
+echo "who is running this: $USER"
+echo "user home directory: $HOME"
