190a288cffcd2b9d682a4b3f669a195f73f1c49c
vijay143-k <vijaykumarmoddu8919@gmail.com>
2025-09-21 23:44:16 +0530

    save the chnages

--- vijay.sh
+++ vijay.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "vijju:: hi kavitha how are u"
-echo "kavitha:: hi vijju i am fine what about you"
-echo "vijju:: i am good thanks for asking what are you doing"
-echo "kavitha:: nothing just watching movies"
+person1=vijju
+person2=kavitha
+
+echo "$person1:: hi $person2 how are u"
+echo "$person2:: hi $person1 i am fine what about you"
+echo "$person1:: i am good thanks for asking what are you doing"
+echo "$person2:: nothing just watching movies"
 
 
