9ec1b07f0d7bf77f05a77c001a5c08461eed7103
vijay143-k <vijaykumarmoddu8919@gmail.com>
2025-09-21 23:50:12 +0530

    save the chnages

---
+++ varible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=trump
+person2=modi
+
+echo "$person1:: hi $person2 how are u"
+echo "$person2:: hi $person1 i am fine what about you"
+echo "$person1:: i am good thanks for asking what are you doing"
+echo "$person2:: nothing just watching movies"
